a0867cb4125839e903c45d39a6e27c3e30be3fb5
Yasmin <yasmin@plugindata.com.br>
2020-08-10 11:49:08 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio14 {
+    class Program { 
+        static void Main(string[] args) {
+
+            int N;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+            for (int i = 0; i < N; i++) { 
+                vet[1] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine(vet[1].ToString("F1", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+        }
+    }
+}
